b2665d347cde0be0b963c173e01e72f3ce124ed5
QPC-Salle <quimperezcastells@mollerussa.lasalle.cat>
2026-02-17 18:55:14 +0100

    BASURA

--- qrExtracted/bootstrap/providers.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    App\Providers\AppServiceProvider::class,
-    App\Services\InvoiceService\InvoiceServiceProvider::class,
-    App\Services\ImageProcessingService\ImageProcessingServiceProvider::class,
-];
